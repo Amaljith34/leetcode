cdabf9e774f48dc87355ae5d84986564567f4f81
Amaljith-aj <jith51541@gmail.com>
2024-10-25 23:32:16 +0530

    35

---
+++ 1347.js
@@ -0,0 +1,18 @@
+// function fn1(a,b){
+//     let count=0
+//     let aa=a.split('').sort().join('');
+//     let bb=b.split('').sort().join('');
+//     if(aa===bb){
+//         return 0
+//     }
+//     let splitedaa=aa.split('')
+//     let splitedbb=bb.split('')
+//     console.log(splitedaa);
+//     for(i=0;i<splitedaa.length;i++){
+        
+//     }
+    
+
+    
+// }
+// console.log(fn1("elslo","olle"));
