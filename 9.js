9d6ac807928e7e069fa0b4574a36395d44997952
AMALJITH K <156279690+Amaljith34@users.noreply.github.com>
2024-11-20 21:53:20 +0530

    Update 9.js

--- 9.js
+++ 9.js
@@ -1,17 +1,39 @@
-var palindromnumber = function (nums) {
-  let reviseArr = [];
-  if (nums < 0) {
-    return false;
-  } else {
-    let sortedArr = nums.toString().split("").map(Number)
-    for (i = sortedArr.length - 1; i >= 0; i--) {
-      reviseArr.push(sortedArr[i]);
-    }
-    if (nums.join('') == reviseArr.join('')) {
-      return true;
-    } else {
-      return false;
-    }
+// var palindromnumber = function (nums) {
+//   let reviseArr = [];
+//   if (nums < 0) {
+//     return false;
+//   } else {
+//     let sortedArr = nums.toString().split("").map(Number)
+//     for (i = sortedArr.length - 1; i >= 0; i--) {
+//       reviseArr.push(sortedArr[i]);
+//     }
+//     if (nums.join('') == reviseArr.join('')) {
+//       return true;
+//     } else {
+//       return false;
+//     }
+//   }
+// };
+// console.log(palindromnumber([-121]));
+
+
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function(x) {
+  if(x<0){
+      return false
+  }   
+  else{
+      let nums=x.toString().split('').reverse().join('')
+   console.log(nums);
+   if(nums===x){
+    return true
+   }
+   else{
+      return false
+   }
+      
   }
 };
-console.log(palindromnumber([-121]));
